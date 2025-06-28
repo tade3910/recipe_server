4b557c9a12e9ae77ded57eaff434c54c1dcd1c61
tade3910 <omotadeogunmodede@gmail.com>
2025-06-28 14:45:19 -0400

    implemented equals method for recipe

--- pkg/models/recipe.go
+++ pkg/models/recipe.go
@@ -1,8 +1,28 @@
 package models
 
+import (
+	"reflect"
+)
+
 type Recipe struct {
 	Url          string     `json:"url" gorm:"primaryKey"`
 	Title        string     `json:"title"`
 	Ingredients  StringList `gorm:"type:jsonb" json:"ingredients"`
 	Instructions StringList `gorm:"type:jsonb" json:"instructions"`
+}
+
+func (actual *Recipe) Equals(other *Recipe) bool {
+	if actual.Url != other.Url {
+		return false
+	}
+	if actual.Title != other.Title {
+		return false
+	}
+	if !reflect.DeepEqual(actual.Ingredients, other.Ingredients) {
+		return false
+	}
+	if !reflect.DeepEqual(actual.Instructions, other.Instructions) {
+		return false
+	}
+	return true
 }
