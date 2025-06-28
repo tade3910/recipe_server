4212492ca16986d1540a965bfd856a917fc1d913
tade3910 <omotadeogunmodede@gmail.com>
2025-06-28 11:21:19 -0400

    added delete and update endpoints

--- pkg/routes/recipe/recipe.go
+++ pkg/routes/recipe/recipe.go
@@ -40,6 +40,41 @@ func (handler *recipeHandler) handleGet(w http.ResponseWriter, url string) {
 	util.RespondWithJSON(w, http.StatusAccepted, recipe)
 }
 
+func (handler *recipeHandler) hanldeDelete(w http.ResponseWriter, url string) {
+	recipe, err := handler.getByUrl(url)
+	if err != nil {
+		util.RespondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+	response := handler.db.Delete(recipe)
+	if response.Error != nil {
+		util.RespondWithError(w, http.StatusInternalServerError, response.Error.Error())
+		return
+	}
+	util.RespondWithJSON(w, http.StatusOK, "Deleted")
+}
+
+func (handler *recipeHandler) handleUpdate(w http.ResponseWriter, r *http.Request, url string) {
+	updateRecipe := &models.Recipe{}
+	err := util.GetBody(r, updateRecipe)
+	if err != nil {
+		util.RespondWithError(w, http.StatusBadRequest, err.Error())
+		return
+	}
+	recipe, err := handler.getByUrl(url)
+	if err != nil {
+		util.RespondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+	updateRecipe.Url = recipe.Url
+	response := handler.db.Save(updateRecipe)
+	if response.Error != nil {
+		util.RespondWithError(w, http.StatusInternalServerError, response.Error.Error())
+		return
+	}
+	util.RespondWithJSON(w, http.StatusAccepted, updateRecipe)
+}
+
 func (handler *recipeHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	url := strings.TrimPrefix(r.URL.Path, "/recipe/")
 	url_split := strings.Split(url, "/")
@@ -53,7 +88,11 @@ func (handler *recipeHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) 
 		}
 		switch r.Method {
 		case http.MethodGet:
-			handler.handleGet(w, url_split[0])
+			handler.handleGet(w, request_url)
+		case http.MethodDelete:
+			handler.hanldeDelete(w, request_url)
+		case http.MethodPut:
+			handler.handleUpdate(w, r, request_url)
 		default:
 			util.RespondWithError(w, http.StatusMethodNotAllowed, "Invalid method")
 		}
